7d5ebd9d7a4554cdac67b68d650defdbd2677e3e
Alberto Diaz Martin <adiazcan@hotmail.com>
2023-06-19 10:51:05 +0200

    demo customer api with plugin manifest

---
+++ customers-api/Program.cs
@@ -0,0 +1,60 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+var customers = new List<Customer>()
+{
+    new Customer(1, "John Doe"),
+    new Customer(2, "Jane Doe"),
+    new Customer(3, "John Smith"),
+    new Customer(4, "Jane Smith")
+};
+
+app.MapGet("customers", () => 
+{
+    return customers;
+})
+.WithName("GetCustomers")
+.WithDescription("Get all customers")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+record Customer(int Id, string Name);
